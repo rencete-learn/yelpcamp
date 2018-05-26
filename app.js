2489401c23570754c189bb5acebcc8672ae5b09a
Terence Yu <rencete@yahoo.com>
2018-05-26 14:35:57 +0800

    Add empty campgrounds page

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, resp) => {
     resp.render("landing");
-})
+});
+
+app.get("/campgrounds", (req, res) => {
+    var campgrounds = [
+        { name: "Salmon Creek", image: "https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
+        { name: "Granite Hill", image: "https://images.pexels.com/photos/939723/pexels-photo-939723.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
+        { name: "Mountain Goat's Rest", image: "https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"}
+    ]
+
+    res.render("campgrounds");
+});
 
 app.listen(8080, () => {
     console.log("Server has started");
-})
+});
